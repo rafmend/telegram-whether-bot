1089f4451745376f1779df7e0ddaf8ce2f9bf067
rafmend <149906249+rafmend@users.noreply.github.com>
2024-10-24 23:30:45 +0200

    Update bot.py
    Added functions for receiving weather satellite images and data. 
    
    Deleted unused methods.

--- bot.py
+++ bot.py
@@ -8,53 +8,45 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 AEMET_TOKEN = os.environ.get('AEMET_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
-sQLite=[]
-
 # Wellcome Message 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "ChatBot Running at "+datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
-# Drop data
-@bot.message_handler(commands=['drop'])
-def drop(message):
-    sent_msg = bot.send_message(message.chat.id, "What do I drop?" ,parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg,drop_handler)
+# Enviar el ultimo informe de una estacion
+@bot.message_handler(commands=['observacion_estacion'])
+def observacion_estacion_pedida(message):
+    sent_msg = bot.send_message(message.chat.id, "¿De qué estación quiere la información? _Valladolid=2422_" ,parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg,observacion_estacion_devolucion)
 
-def drop_handler(message):
-    sQLite.append(message.text)
-    bot.send_message(message.chat.id, "Saved in position " + str(len(sQLite)))
-
-# Pop data
-@bot.message_handler(commands=['pop'])
-def pop(message):
-    if len(sQLite) == 0:
-        bot.send_message(message.chat.id, "Cannot pop from empty stack")
-    else:
-        bot.send_message(message.chat.id, "Pop from position "+ str(len(sQLite)) + ": " + sQLite.pop(len(sQLite)-1))
-
-# Sends the last whether report (TODO: Input location)
-@bot.message_handler(commands=['whether'])
-def whether(message):
+def observacion_estacion_devolucion(message):
     conn = http.client.HTTPSConnection("opendata.aemet.es")
     headers = {
         'cache-control': "no-cache"
         }
-    conn.request("GET", "/opendata//api/observacion/convencional/datos/estacion/2422?api_key="+AEMET_TOKEN, headers=headers)
-
+    conn.request("GET", "/opendata//api/observacion/convencional/datos/estacion/"+message.text+"?api_key="+AEMET_TOKEN, headers=headers)
     res = conn.getresponse()
     data = res.read()
     entrada1=json.loads(data.decode("utf-8"))
+    
     conn.request("GET", entrada1['datos'][25:100], headers=headers)
     res2 = conn.getresponse()
     data2 = res2.read()
     entrada2=json.loads(data2.decode("utf-8"))[0]
-    bot.send_message(message.chat.id, "*Location: *" + entrada2['ubi'] + "\n*Date of Data:* " 
-    + entrada2['fint'] + "\n*Temperature:* " + str(entrada2['ta']) + "ºC\n*Humidity:* " + str(entrada2['hr']) +
-    "%\n*Accumulated Precipitation:* " + str(entrada2['prec']) + "l/m^2\n*Average Wind Speed:* " + str(entrada2['vvu']) + 
-    "m/s\n*Pressure:* " + str(entrada2['pres']) + "hPa\n*Visibility:* " + str(entrada2['vis']) + "Km", parse_mode= 'Markdown')
 
-# Sends the last radar image (TODO: Input location)
+    conn.request("GET", entrada1['metadatos'][25:200], headers=headers)
+    res3 = conn.getresponse()
+    data3 = res3.read()
+    entrada3=json.loads(data3.decode("windows-1250","ignore"))
+    #print(entrada3['campos'][0])
+    salida = ""
+    for i in entrada2:
+        for j in entrada3['campos']:
+            if j['id']==i:
+                bot.send_message(message.chat.id, j['descripcion']+":\n"+str(entrada2[i]))
+    
+
+# Sends the last radar image from Palencia
 @bot.message_handler(commands=['radar'])
 def radar(message):
     conn = http.client.HTTPSConnection("opendata.aemet.es")
@@ -70,27 +62,96 @@ def radar(message):
     data2 = res2.read()
     bot.send_photo(message.chat.id,data2)
 
+@bot.message_handler(commands=['presion'])
+def presion(message):
+    conn = http.client.HTTPSConnection("opendata.aemet.es")
+    headers = {
+        'cache-control': "no-cache"
+        }
+    conn.request("GET", "/opendata/api/mapasygraficos/analisis/?api_key="+AEMET_TOKEN, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    entrada1=json.loads(data.decode("utf-8"))
+    conn.request("GET", str(entrada1['datos'])[25:100], headers=headers)
+    res2 = conn.getresponse()
+    data2 = res2.read()
+    bot.send_photo(message.chat.id,data2)
 
+@bot.message_handler(commands=['rayos'])
+def rayos(message):
+    conn = http.client.HTTPSConnection("opendata.aemet.es")
+    headers = {
+        'cache-control': "no-cache"
+        }
+    conn.request("GET", "/opendata/api/red/rayos/mapa/?api_key="+AEMET_TOKEN, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    entrada1=json.loads(data.decode("utf-8"))
+    conn.request("GET", str(entrada1['datos'])[25:100], headers=headers)
+    res2 = conn.getresponse()
+    data2 = res2.read()
+    bot.send_photo(message.chat.id,data2)
 
-# Add two numbers 
-@bot.message_handler(commands=['sum'])
-def send_sum(message):
-    sent_msg = bot.send_message(message.chat.id, "What numbers do I add?:\n\n Num 1:" ,parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg,sum_handler)
+@bot.message_handler(commands=['incendios'])
+def incendios(message):
+    conn = http.client.HTTPSConnection("opendata.aemet.es")
+    headers = {
+        'cache-control': "no-cache"
+        }
+    conn.request("GET", "/opendata/api/incendios/mapasriesgo/estimado/area/p/?api_key="+AEMET_TOKEN, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    entrada1=json.loads(data.decode("utf-8"))
+    conn.request("GET", str(entrada1['datos'])[25:100], headers=headers)
+    res2 = conn.getresponse()
+    data2 = res2.read()
+    bot.send_photo(message.chat.id,data2)
 
-def sum_handler(message):
-    num1=message.text
-    sent_msg = bot.send_message(message.chat.id, "Num 2:" ,parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg, sum_handler2, num1)
+@bot.message_handler(commands=['prediccion_hoy'])
+def prediccion_hoy(message):
+    sent_msg = bot.send_message(message.chat.id, "¿De qué provincia quiere la información? _Valladolid=47_" ,parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg,prediccion_hoy_devolucion)
 
-def sum_handler2(message,num1):
-    num2=message.text
-    num=int(num1)+int(num2)
-    bot.send_message(message.chat.id,"Sum is " + num1 + "+" + num2 + " = " + str(num))
+def prediccion_hoy_devolucion(message):
+    conn = http.client.HTTPSConnection("opendata.aemet.es")
+    headers = {
+        'cache-control': "no-cache"
+        }
+    conn.request("GET", "/opendata/api/prediccion/provincia/hoy/"+message.text+"?api_key="+AEMET_TOKEN, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    entrada1=json.loads(data.decode("utf-8"))
+    conn.request("GET", str(entrada1['datos'])[25:100], headers=headers)
+    res2 = conn.getresponse()
+    data2 = res2.read()
+    bot.send_message(message.chat.id, data2.decode("windows-1250","ignore"))
+
+@bot.message_handler(commands=['prediccion_manana'])
+def prediccion_manana(message):
+    sent_msg = bot.send_message(message.chat.id, "¿De qué provincia quiere la información? _Valladolid=47_" ,parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg,prediccion_manana_devolucion)
+
+def prediccion_manana_devolucion(message):
+    conn = http.client.HTTPSConnection("opendata.aemet.es")
+    headers = {
+        'cache-control': "no-cache"
+        }
+    conn.request("GET", "/opendata/api/prediccion/provincia/manana/"+message.text+"?api_key="+AEMET_TOKEN, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    entrada1=json.loads(data.decode("utf-8"))
+    conn.request("GET", str(entrada1['datos'])[25:100], headers=headers)
+    res2 = conn.getresponse()
+    data2 = res2.read()
+    bot.send_message(message.chat.id, data2.decode("windows-1250","ignore"))
+
 
 # Last resort
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    bot.reply_to(message, "Ni idea que me dices illo")
+    bot.reply_to(message, "Unregistered command")
+
+
+
 
 bot.infinity_polling()
